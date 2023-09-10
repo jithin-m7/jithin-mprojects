3ddb301c0bf1b9ddf1175232ff8c73b1243707bf
jithin-m7 <144557436+jithin-m7@users.noreply.github.com>
2023-09-11 01:24:21 +0530

    Update fileuploadanddownlod.java

--- src/projects/fileuploadanddownlod.java
+++ src/projects/fileuploadanddownlod.java
@@ -1,5 +1,20 @@
 package projects;
 
 public class fileuploadanddownlod {
+  ChromeDriver driver;
+	@Before
+	public void start()
+	{
+		driver=new ChromeDriver();
+		driver.get("https://www.techlistic.com/p/selenium-practice-form.html");
+	}
+	
+	@Test
+	public void test() throws InterruptedException 
+	{
+	
+		 driver.findElement(By.xpath("//*[@id=\"photo\"]")).sendKeys("C:\\Users\\user\\Downloads\\JithinMResume.pdf");
+		 driver.findElement(By.xpath("//*[@id=\"post-body-3077692503353518311\"]/div[1]/div/div/h2[2]/div[1]/div/div[28]/div[2]/div/a")).click();
+	}
 
 }
