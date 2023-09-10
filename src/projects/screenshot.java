9441205a568f90c0718b9086dbba9dafcb42b4b3
jithin-m7 <144557436+jithin-m7@users.noreply.github.com>
2023-09-11 01:28:06 +0530

    Update screenshot.java

--- src/projects/screenshot.java
+++ src/projects/screenshot.java
@@ -1,5 +1,23 @@
 package projects;
 
 public class screenshot {
+  ChromeDriver driver ;
+	@Before
+	public void b()
+	{
+	driver=new ChromeDriver();
+	driver.get("https://www.techlistic.com/p/selenium-practice-form.html");
+}
+
+	
+		
+	
+	@Test
+	public void testBStackTakeScreenShot() throws Exception{
+
+		File f=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		Files.copy(f,new File("C:\\Users\\user\\Downloads\\sample\\scennshoth.jpg"));
+	}
+
 
 }
