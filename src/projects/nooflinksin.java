bb7886e249aa8b2cc542eef5573e02051047cc8f
jithin-m7 <144557436+jithin-m7@users.noreply.github.com>
2023-09-11 01:26:23 +0530

    Update nooflinksin.java

--- src/projects/nooflinksin.java
+++ src/projects/nooflinksin.java
@@ -1,5 +1,33 @@
 package projects;
 
 public class nooflinksin {
+  ChromeDriver driver;
+
+	@Before
+	public void start()
+	{
+		driver=new ChromeDriver();
+		driver.get("https://www.techlistic.com/p/selenium-practice-form.html");
+	}
+	
+	@Test
+	public void test()
+	{
+		List<WebElement> li= driver.findElements(By.tagName("a"));
+	 	System.out.println("total no of links"+li.size());
+	 	
+	 	for(WebElement s:li)
+	 	{
+	 		//System.out.println(s);
+	 		String h=s.getAttribute("href");
+	 		System.out.println(h); 
+	 		
+	 		String t=s.getText();  
+	 		System.out.println(t);
+	 		
+	 		
+	 	}
+
+	}
 
 }
